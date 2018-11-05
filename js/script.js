a4465355d3f8fe32ce22021f4e383155976a8ab3
Ralph Perdomo <ralph@prdmo.com>
2018-11-05 11:58:52 -0800

    begin to make ndbno calls

--- js/script.js
+++ js/script.js
@@ -1,49 +1,46 @@
 // start with search to get NBDNO number:
 //
 //
-//  https://api.nal.usda.gov/ndb/search/?format=json&q=0722252601445&sort=n&max=25&offset=0&api_key=mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP
+//
+ https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&&ds='Standard Reference'max=25&offset=0&api_key=mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP
 //
 //  then run basic report:
 //
 // https://api.nal.usda.gov/ndb/V2/reports?ndbno=45239291&&type=b&format=json&api_key=mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP
 //
 //
+// start here https://ndb.nal.usda.gov/ndb/doc/apilist/API-FOOD-REPORTV2.md
 
+function nbSearch(q) {
+  const apiKey = `mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP`;
+  let url = `https://api.nal.usda.gov/ndb/search/?format=json&q=${q}&sort=n&&ds='Standard Reference'max=25&offset=0&api_key=${apiKey}`
 
+  fetch(url).then(response => console.log(response))
 
-// fetch('https://api.nal.usda.gov/ndb/reports/?ndbno=01009&type=b&format=xml&api_key=DEMO_KEY')
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(myJson) {
-//     console.log(JSON.stringify(myJson));
-//   });
-//
-//
-//
-//   fetch('https://api.nal.usda.gov/ndb/search/?format=json&q=0722252601445&sort=n&max=1&offset=0&api_key=mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP').then(response=>response.json()).then(x=>console.log(x))
-//
-//
-//
-//   fetch('https://api.nal.usda.gov/ndb/V2/reports?ndbno=45239291&&type=b&format=json&api_key=mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP').then(response=>response.json()).then(x=>console.log(x))
-//
+}
 
-  fetch('https://api.nal.usda.gov/ndb/V2/reports?ndbno=45239291&&type=b&format=json&api_key=mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP').then(response=>response.json()).then(x=>console.log(x.foods[0].food.nutrients))
 
 $(function() {
+  $("#ndbno").keyup(function() {
+    console.log($("#ndbno").val())
+    nbSearch($("#ndbno").val());
+  })
 
   $("#submit").click(function() {
     let foodItem = $("#ndbno").val()
     let foodBuilder = `https://api.nal.usda.gov/ndb/V2/reports?ndbno=${foodItem}&&type=b&format=json&api_key=mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP`
+
+    let foodResponse;
 
     // 45239291
 
     console.log(foodItem)
 
     fetch(`https://api.nal.usda.gov/ndb/V2/reports?ndbno=${foodItem}&&type=b&format=json&api_key=mhNpLtFVjwZ2FqugLnJYF8T4cCKIhiIGTAGOVSvP`).then(response=>response.json()).then((response)=>{
-      console.log(response.foods[0].food)
+      foodResponse = response.foods[0].food;
+      console.log(response.foods[0].food);
 
-      // $(".output").append(response.foods[0].food.nutrients)
+      $(".output").append(foodResponse.desc.name)
       // $(".output").append(response.foods[0].food.nutrients)
     });
 
